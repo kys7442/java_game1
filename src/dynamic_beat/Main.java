c902dfe6e2b9989bfb0b43fbabd750d3453572aa
Kim Young Sik <kys7442@naver.com>
2017-09-18 18:13:55 +0900

    메뉴바 만들기

---
+++ src/dynamic_beat/Main.java
@@ -0,0 +1,16 @@
+package dynamic_beat;
+
+public class Main {
+	
+	public static final int SCREEN_WIDTH = 1280;
+	public static final int SCREEN_HEIGHT = 720;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new DynamicBeat();
+		
+		//http://wallpaperswide.com - 무료 이미지 
+		//www.javazoom.net - 자바 mp3 라이브러리
+	}
+
+}
